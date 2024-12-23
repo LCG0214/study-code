76704a3032dfd1a75e1f6800193598299db074a4
LCG <97706396@qq.com>
2024-12-23 16:10:05 +0800

    新增一个算法题：轮转数组

--- Algorithm/src/leetcode/PracticeTest.java
+++ Algorithm/src/leetcode/PracticeTest.java
@@ -11,12 +11,58 @@ import java.util.Arrays;
  */
 public class PracticeTest {
 
+    /** 2024-12-23
+     * 轮转数组
+     * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+     * 示例 1:
+     * 输入: nums = [1,2,3,4,5,6,7], k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右轮转 1 步: [7,1,2,3,4,5,6]
+     * 向右轮转 2 步: [6,7,1,2,3,4,5]
+     * 向右轮转 3 步: [5,6,7,1,2,3,4]
+     * 示例 2:
+     * 输入：nums = [-1,-100,3,99], k = 2
+     * 输出：[3,99,-1,-100]
+     * 解释:
+     * 向右轮转 1 步: [99,-1,-100,3]
+     * 向右轮转 2 步: [3,99,-1,-100]
+     * 个人思路：（未通过，有一个测试用例超时）循环k次，每次循环取出数组最后一位，其他数据往后移动一位，再把最后一位放到数组最前面
+     * 官方思路：使用一个新数组存储 循环k次 放到新数组的  (i + k) % n 位置
+     */
+
+    @Test
+    public void practice6() {
+        // 输入: nums = [1,2,3,4,5,6,7], k = 3
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 54944;
+        // 具体逻辑
+        for (int i = 0; i < k; i++) {
+            int lastData = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = lastData;
+        }
+        // 官方解法
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+
     /** 2024-12-11
      * 多数元素
      * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
      * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
      *
-     * 思路：双层遍历，数组中的每个元素从角标0开始计数，超过阈值直接记录。
+     * 思路：（效果不如官方）双层遍历，数组中的每个元素从角标0开始计数，超过阈值直接记录。
      * 官解地址：https://leetcode.cn/problems/majority-element/solutions/?envType=study-plan-v2&envId=top-interview-150
      */
     @Test
